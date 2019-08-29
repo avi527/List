393725071debe9efa52698b3ccfdda5542adacee
avi527 <54246200+avi527@users.noreply.github.com>
2019-08-29 18:05:25 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,8 @@
+'''
+count()	returns occurrences of element in a list
+'''
+thisList=['a','b','b','b','b','b','v',1,2,3,4]
+a=thisList.count('b')
+print(a)
+
+#NOTE :- count use for find repeted value in list
