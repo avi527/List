45c8908bfce3d4bcfae9751ffe4e6c8ef945bf23
avi527 <54246200+avi527@users.noreply.github.com>
2019-08-29 18:15:12 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,16 @@
+'''
+sort()	sorts elements of a list
+'''
+list=['ram','sita','gita']
+list.sort()
+print(list)
+
+numbers = [1, 3, 4, 2]
+
+# Sorting list of Integers in ascending
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+numbers.sort()
+print(numbers)
