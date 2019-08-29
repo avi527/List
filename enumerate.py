ae6ceec93f2ad7ce86efc9c855839e56b28df373
avi527 <54246200+avi527@users.noreply.github.com>
2019-08-29 18:48:06 +0530

    Add files via upload

---
+++ enumerate.py
@@ -0,0 +1,10 @@
+list=['ram','sita','gita']
+enu_list=enumerate(list)
+print(enu_list)
+for i in enu_list:
+    print(i)
+#enumerate function always return object after object
+# iterable return tuple
+
+
+
