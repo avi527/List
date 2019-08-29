6a47f8e145c165a252400c0f98d1c3a56d815f7b
avi527 <54246200+avi527@users.noreply.github.com>
2019-08-29 17:23:00 +0530

    Add files via upload

---
+++ extend.py
@@ -0,0 +1,17 @@
+'''
+extend() -> Add Elements of a List to Another List
+'''
+list1=['life','so']
+list2=['hard']
+list1.extend(list2)
+print(list1)
+
+#see string are split
+list_a=['hii','how','are']
+list_a.extend('split string')
+print(list_a)
+
+
+a=slice(3,15,)
+b=list_a[a]
+print(b)
